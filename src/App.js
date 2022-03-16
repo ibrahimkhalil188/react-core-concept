239efed7bd8ec81ec4bd9fd4b13548518911b0ce
unknown <ibrahimkhalil.mine@gmail.com>
2022-03-16 06:57:46 +0600

    props

--- src/App.js
+++ src/App.js
@@ -5,30 +5,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      <Person name="Tamim Iqbal" roll="batsman"></Person>
+      <Person name="Sakib al hasan" roll="all rounder"></Person>
+      <Person name="Taskin Ahmed" roll="bowler"></Person>
+      <Person name="Sikander raja" roll="ampiare"></Person>
       <h1>Hello Fan I'm from south africa
       </h1>
-      <Friend></Friend>
+      <Friend job="survey" type="freelance"></Friend>
     </div>
   );
 }
 
-function Person() {
+function Person(props) {
   return (
     <div className='person'>
-      <h1>Sakib all hasan</h1>
-      <p>All rounder</p>
+      <h1>{props.name}</h1>
+      <p>{props.roll}</p>
     </div>
   )
 }
-function Friend() {
+function Friend(props) {
   return (
     <div className='person'>
-      <h1>Tamim Khan</h1>
-      <p>I hate them</p>
+      <h1>{props.job}</h1>
+      <p>{props.type}</p>
     </div>
   )
 }
